35f59bb42da21cea24ac8c8204bf4923bc21dca4
Daniel dos Santos Araujo <denenewton@icloud.com>
2022-08-06 21:05:19 -0300

    add metodo paginaCorrente

--- site-movie/js/components.js
+++ site-movie/js/components.js
@@ -2,46 +2,56 @@ const movieDiv = document.querySelector(".movies");
 const searchElem = document.getElementById("search");
 
 export function movieHtml(results) {
-    movieDiv.innerHTML = `${results
-        .map((mov) => {
-            return `
+  movieDiv.innerHTML = `${results
+    .map((mov) => {
+      return `
                     <div class="movie">
                         <div class="image">
                             <img src="${mov.image}" alt=" image ${mov.name}">
                         </div>
                         <div class="description">
-                            <h2><a href="${mov.url}"> ${mov.name}</a></h2>
+                            <h2><a href="${mov.url}" target="_blanck"> ${mov.name}</a></h2>
                             <h4>Diretor: <span>${mov.director}</span></h4>
                             <p>${mov.comments}</p>
                         </div>
                     </div>
             
                 `;
-        })
-        .join("")}`;
+    })
+    .join("")}`;
 }
 
 export const filtro = (m) => {
-    const firstChar = m.name.split("");
-    const search = searchElem.value;
-    if (search.trim() === '') return []; //retorna o vetor original 
-    if (firstChar[0].toLocaleLowerCase() === search.split("")[0].toLowerCase()) {
-      return m;
-    }else return //retorna um vetor vazio
-  };
- 
-
+  const firstChar = m.name.split("");
+  const search = searchElem.value;
+  if (search.trim() === "") return []; //retorna o vetor original
+  if (firstChar[0].toLocaleLowerCase() === search.split("")[0].toLowerCase()) {
+    return m;
+  } else return; //retorna um vetor vazio
+};
 
 export function paginate(vetor, pages, limits) {
   const page = pages;
   const limit = limits;
-
   const startIndex = (page - 1) * limit;
   const endIndex = page * limit;
 
   let results = [];
   results = vetor.slice(startIndex, endIndex);
-  
+
   return results;
 }
 
+export function paginaCorrente(data, page, limit) {
+  movieHtml(paginate(data, page, limit));
+  
+  $("#nextPage").click(function () {
+    page++;
+    movieHtml(paginate(data, page, limit));
+  });
+
+  $("#prevPage").click(function () {
+    if (page > 1) page--;
+    movieHtml(paginate(data, page, limit));
+  });
+}
--- site-movie/js/index.js
+++ site-movie/js/index.js
@@ -2,28 +2,23 @@ import { data } from "./data.js";
 import * as component from "./components.js";
 
 $(document).ready(function () {
-  var page = 1;
-  const limt = 4;
-  component.movieHtml(component.paginate(data, page, limt));
   $("#search").focus(); //mantem o focus em search
+  var page = 1;
+  const limit = 4;
 
-  $("#nextPage").click(function () {
-    page++;
-    component.movieHtml(component.paginate(data, page, limt));
-  });
+  component.paginaCorrente(data,page,limit)
 
-  $("#prevPage").click(function () {
-    if (page > 1) page--;
-    component.movieHtml(component.paginate(data, page, limt));
-  });
 
   $("#search").change(function () {
+
     const results = data.filter(component.filtro);
+
     $(".changePages").hide(); // ocultar o link nextPage
 
     if (results.length === 0) {
       $(".movies").html("<h3>Movie not found in data</h3>");
-      $("#search").val(""); // limpa o valor do compo em search
+      $("#search").val(""); 
+      
     }
     if (results.length === data.length) {
       $(".movies").html();
