8a76deb1f7cfd624d3a7d49d870079d787fa3e0d
Daniel dos Santos Araujo <denenewton@icloud.com>
2022-08-18 17:13:45 -0300

    add

--- site-movie/js/data.js
+++ site-movie/js/data.js
@@ -1,9 +1,11 @@
-export async function dataMovies() {
+export const dataMovies = async () => {
   try {
-    const response = await fetch("https://62f58457ac59075124d272a8.mockapi.io/api/v1/movie");
-    const data = response.json();
-    return data;
+    const response = await fetch(
+      'https://62f58457ac59075124d272a8.mockapi.io/api/v1/movie'
+    )
+    const data = response.json()
+    return data
   } catch (e) {
-    console.error(e);
+    console.error(e)
   }
 }
--- site-movie/js/index.js
+++ site-movie/js/index.js
@@ -1,41 +1,41 @@
-import { dataMovies } from "./data.js";
-import * as component from "./components.js";
+import { dataMovies } from './data.js'
+import * as component from './components.js'
 
-$(".changePages").hide();
+$('.changePages').hide()
 
 $(document).ready(async function () {
-  var data = await dataMovies();
-  $(".changePages").show();
-  $("#search").focus();
-  var page = 1;
-  const limit = 4;
+  var data = await dataMovies()
+  $('.changePages').show()
+  $('#search').focus()
+  var page = 1
+  const limit = 4
 
-  component.paginaCorrente(data, page, limit);
+  component.paginaCorrente(data, page, limit)
 
-  $("#search").change(function () {
-    $(".changePages").hide();
-    $(".movies").html("");
+  $('#search').change(function () {
+    $('.changePages').hide()
+    $('.movies').html('')
 
-    let termo = this.value;
-    let expressaoReg = new RegExp(termo.trim(), "i");
+    let termo = this.value
+    let expressaoReg = new RegExp(termo.trim(), 'i')
 
-    const results = data.filter((elem) => {
-      return expressaoReg.test(elem.name);
-    });
+    const results = data.filter(elem => {
+      return expressaoReg.test(elem.name)
+    })
 
-    $(".changePages").hide(); // ocultar o link nextPage
+    $('.changePages').hide() // ocultar o link nextPage
 
     if (results.length === 0) {
-      $(".movies").html("<h3 id='not-found'>Movie not found in data</h3>");
-      $("#search").val("");
+      $('.movies').html("<h3 id='not-found'>Movie not found in data</h3>")
+      $('#search').val('')
     }
     if (results.length === data.length) {
-      component.movieHtml(component.paginate(data, 1, 4));
-      $(".changePages").show();
+      component.movieHtml(component.paginate(data, 1, 4))
+      $('.changePages').show()
     }
     if (results.length < data.length && results.length > 0) {
-      component.movieHtml(results);
-      $("#search").val("");
+      component.movieHtml(results)
+      $('#search').val('')
     }
-  });
-});
+  })
+})
